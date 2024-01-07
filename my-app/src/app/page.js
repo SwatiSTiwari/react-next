50c81b010838fceba5e4a37ccbb85272074acba7
SwatiSTiwari <swatistiwati13@gmail.com>
2024-01-07 19:43:39 +0530

    started with next

---
+++ my-app/src/app/page.js
@@ -0,0 +1,39 @@
+// "use client";
+// import React, { useContext } from 'react';
+// import { UsernameContext } from './Helper/Context.js';
+// import Header from './Component/Header.js';
+
+// const Page = () => {
+//   const user = useContext(UsernameContext);
+// console.log("user",user)
+//   // Rest of the component code...
+
+//   return (
+//     <>
+//       <h1>{user}</h1>
+//       <Header />
+//       {/* JSX code for the component... */}
+//     </>
+//   );
+// };
+
+
+
+// export default Page;
+// "use client";
+// import React from 'react';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// const Page = () => {
+//   const notify = () => toast("Wow so easy!");
+
+//   return (
+//     <div>
+//       <button onClick={notify}>Notify!</button>
+//       <ToastContainer />
+//     </div>
+//   );
+// };
+
+// export default Page
